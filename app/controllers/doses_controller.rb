d780ec72325497dc978efd3643d9b597af90fb16
Sara Scaini <sa.scaini@gmail.com>
2019-02-22 13:57:08 +0000

    pushing to heroku

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -13,7 +13,8 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
-    redirect_to cocktail_path
+
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,32 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Cocktail.destroy_all
-Ingredient.destroy_all
+# Cocktail.destroy_all
+# Ingredient.destroy_all
 
-lemon = Ingredient.create(name: "lemon")
-ice = Ingredient.create(name: "ice")
-mint = Ingredient.create(name: "mint leaves")
+# lemon = Ingredient.create(name: "lemon")
+# ice = Ingredient.create(name: "ice")
+# mint = Ingredient.create(name: "mint leaves")
 
-gin_tonic = Cocktail.create(name: "Gin&Tonic")
-mojito = Cocktail.create(name: "Mojito")
+# gin_tonic = Cocktail.create(name: "Gin&Tonic")
+# mojito = Cocktail.create(name: "Mojito")
 
-Dose.create(cocktail: mojito, ingredient: lemon, description: "Lemon" )
+# Dose.create(cocktail: mojito, ingredient: lemon, description: "Lemon")
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+ingredients = JSON.parse(user_serialized)
+
+ingredients['drinks'].each do |ingredient|
+  ing = Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+
+
+
+
+
+
