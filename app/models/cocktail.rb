8479e123d2392c1cd2847176b40bc023e2843bea
Sara Scaini <sa.scaini@gmail.com>
2019-03-24 19:02:32 +0000

    added photo validation

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+  validates :photo, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   mount_uploader :photo, PhotoUploader
